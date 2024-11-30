9fb260f577fe38b60b3718d1bf852c71e3cb6e92
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2024-11-30 11:08:37 +0700

    Update IP

--- app/src/main/java/com/example/qgeni/data/repository/DefaultConnection.kt
+++ app/src/main/java/com/example/qgeni/data/repository/DefaultConnection.kt
@@ -3,7 +3,7 @@ package com.example.qgeni.data.repository
 
 internal object DefaultConnection {
 
-    const val HOST = "10.tcp.ap.ngrok.io"
+    const val HOST = "0.tcp.ap.ngrok.io"
     const val PORT = 15506
 //    const val HOST = "192.168.1.173"
 //    const val PORT = 27017
