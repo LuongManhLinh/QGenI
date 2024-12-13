48cd4232d50dc9c40bceb57eaebca9073e68d8c8
LuongManhLinh <luongmanhlinh2004cb@gmail.com>
2024-12-13 14:05:07 +0700

    Update Host Port in GD3

--- app/src/main/java/com/example/qgeni/data/DefaultConnection.kt
+++ app/src/main/java/com/example/qgeni/data/DefaultConnection.kt
@@ -4,7 +4,7 @@ package com.example.qgeni.data
 object DefaultConnection {
     const val DB_NAME = "qgeni"
 //    const val HOST = "0.tcp.ap.ngrok.io"
-    const val HOST = "192.168.1.173"
+    const val HOST = "10.10.68.186"
     var dbPort = 27017
     var genPort = 20000
     var ctrlPort = 20001
